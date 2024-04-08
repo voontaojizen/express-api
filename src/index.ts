ba39c834b7fea385401bbe7766c5c036bee75165
voontaojizen <voontao@jizen.net>
2024-04-08 19:42:38 +0800

    a

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+type Blog = {
+  id: string;
+  title: string;
+  content: string;
+}
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({ origin: '*' }));
+
+app.get('/blogs', (req: Request, res: Response) => {
+  const blogs: Blog[] = [
+    { id: '1', title: 'Blog 1', content: 'Content 1' },
+    { id: '2', title: 'Blog 2', content: 'Content 2' },
+    { id: '3', title: 'Blog 3', content: 'Content 3' },
+    { id: '4', title: 'Blog 4', content: 'Content 4' },
+  ];
+  res.json({ blogs });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
