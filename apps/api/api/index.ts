86f0d2703a6e3d32ebfbea1d5b4d59f5f87386dd
voontaojizen <voontao@jizen.net>
2024-04-08 23:23:32 +0800

    .

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -17,6 +17,10 @@ app.use(cors({ origin: '*' }));
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
 router.get('/blogs', (req: Request, res: Response) => {
   const blogs: Blog[] = [
     { id: '1', title: 'Blog 1', content: 'Content 1' },
@@ -29,6 +33,6 @@ router.get('/blogs', (req: Request, res: Response) => {
 
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
